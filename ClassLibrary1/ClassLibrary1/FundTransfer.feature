61b7d75c2b5f7a9ec8a4fc64ffd3d8bbe83360b4
Dixita Parmar <dixita.parmar@bjss.com>
2015-01-23 16:50:08 +0000

    Checking in first specflow project

---
+++ ClassLibrary1/ClassLibrary1/FundTransfer.feature
@@ -0,0 +1,12 @@
+﻿Feature: Customer Transfers Fund
+	As a customer,
+	I want to transfer funds
+	so that I can send money to my friends and family
+
+@mytag
+Scenario: Valid Payee
+	Given the user is on Fund Transfer Page
+	When he enters "Jim" as payee name
+	And he enters "100" as amount
+	And he Submits request for Fund Transfer
+	Then ensure the fund transfer is complete with "$100 transferred successfully to Jim!!" message
---
+++ ClassLibrary1/ClassLibrary1/FundTransferStepDefs.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+
+namespace FundTransfer.specs
+{
+
+    [Binding]
+    public class FundTransferStepDefs
+    {
+        FundTransferPage _ftPage = new FundTransferPage(Environment.Driver);
+
+
+        [Given(@"the user is on Fund Transfer Page")]
+        public void GivenUserIsOnFundTransferPage()
+        {
+            Environment.Driver.Navigate().GoToUrl("https://dl.dropboxusercontent.com/u/55228056/fundTransfer.html");
+        }
+
+        [When(@"he enters ""(.*)"" as payee name")]
+        public void WhenUserEneteredIntoThePayeeNameField(string payeeName)
+        {
+            _ftPage.payeeNameField.SendKeys(payeeName);
+        }
+        
+        [When(@"he enters ""(.*)"" as amount")]
+        public void WhenUserEneteredIntoTheAmountField(string amount)
+        {
+            _ftPage.amountField.SendKeys(amount);
+        }
+
+        [When(@"he Submits request for Fund Transfer")]
+        public void WhenUserPressTransferButton()
+        {
+            _ftPage.transferButton.Click();
+        }
+
+        [Then(@"ensure the fund transfer is complete with ""(.*)"" message")]
+        public void ThenFundTransferIsComplete(string message)
+        {
+            Assert.AreEqual(message, _ftPage.messageLabel.Text);
+        }
+    }
+}
+
